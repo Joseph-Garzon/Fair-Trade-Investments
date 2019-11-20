fa68fed927aa9183483132588e2926de275acc19
Joseph-Garzon <55984853+Joseph-Garzon@users.noreply.github.com>
2019-11-20 07:53:10 -0600

    Update FTISignIn.js

--- FTISignIn.js
+++ FTISignIn.js
@@ -1,22 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   // your code here
-var name= prompt("Username:");
-var pass= prompt("Password:");
+var u=document.getElementById("username");
+var p=document.getElementById("password");
 var arr_u=["joseph","carlos","tully"];
 var arr_p=["garzon","dilly","turtle"];
 function check(){
+	u=document.getElementById("username");
+    p=document.getElementById("password");
+	var us=u.value;
+    var pa=p.value;
 	for(var x=0;x<arr_u.length;x++){
-		if(arr_u[x]===name){
-			if(arr_p[x]===pass){
+		if(arr_u[x]===us){
+			if(arr_p[x]===pa){
 				alert("Sign in successful!");
 			}else{
 				alert("Incorrect password.");
 			}
 		}
 	}
-	if(!arr_u.includes(name)){
+	if(!arr_u.includes(us)){
 		alert("That username does not exist, try making an account.");
 	}
 }
-check();
+document.getElementById("check").onclick = function changeContent() {
+   check();
+}
 });
