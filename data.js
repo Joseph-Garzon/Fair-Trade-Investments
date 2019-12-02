280196b26189f7ee0a231631ea2af742818a3b37
Joseph-Garzon <55984853+Joseph-Garzon@users.noreply.github.com>
2019-12-01 18:31:28 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+var jospeh={
+	LName:"Garzon",
+	FName:"Jospeh",
+	age:96,
+	CC#:"0000-0000-0000",
+	SS#:"000-00-0000",
+	account#:999
+};
+
+var carlos={
+	LName:"Espinoza",
+	FName:"Carlos",
+	age:2,
+	CC#:"1111-1111-1111",
+	SS#:"999-99-9999",
+	account#:998
+};
+
+var smallBrain={
+	LName:"Something",
+	FName:"Ahbi",
+	age:0,
+	CC#:"2222-2222-2222",
+	SS#:"888-88-8888",
+	account#:991
+};
+
+var accounts=[joseph,carlos,smallBrain];
