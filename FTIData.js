d498e94b709e37dd018aed8c1a61ba91c1cc177a
carlosespinozablunda <55984876+carlosespinozablunda@users.noreply.github.com>
2019-12-01 22:53:32 -0600

    Add files via upload

---
+++ FTIData.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  //your code here
+// var jospeh={4
+// 	LName:"Garzon",
+// 	FName:"Jospeh",
+// 	age:96,
+// 	CCNum:"0000-0000-0000",
+// 	SSNum:"000-00-0000",
+// 	accountNum:999
+// };
+
+var carlos={
+	LName:"Espinoza",
+	FName:"Carlos",
+	age:2,
+	CCNum:"1111-1111-1111",
+	SSNum:"999-99-9999",
+	accountNum:998
+};
+
+var smallBrain={
+	LName:"Something",
+	FName:"Ahbi",
+	age:0,
+	CCNum:"2222-2222-2222",
+	SSNum:"888-88-8888",
+	accountNum:991
+};
+
+var accounts=[carlos,smallBrain];
+
+for (var i = 0; i < accounts.length; i++) {
+	$('#'+i+' #accountNum').text(accounts[i].accountNum);
+	$('#'+i+' #lastName').text(accounts[i].LName);
+	$('#'+i+' #firstName').text(accounts[i].FName);
+	$('#'+i+' #age').text(accounts[i].age);
+	$('#'+i+' #creditNum').text(accounts[i].CCNum);
+	$('#'+i+' #socSecNum').text(accounts[i].SSNum);
+
+}
+
+});
